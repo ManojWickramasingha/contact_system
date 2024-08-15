1f3c45a5ce5f2158a22d5aef61a046ca407b8f95
Manoj wickramasingha <141001887+ManojWickramasingha@users.noreply.github.com>
2024-08-15 08:35:41 +0530

    first commit

---
+++ src/main/java/Loader.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Loader extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/add_contact_form.fxml")))));
+        stage.show();
+    }
+}
